6a0cc3315240e421364792088021f7c794b33cf7
Prashanth Balasubramaniam <prashanth.bala9@gmail.com>
2013-10-28 02:25:37 -0400

    adding class Food Entry

---
+++ Free Food CMU/FoodEntry.h
@@ -0,0 +1,19 @@
+//
+//  FoodEntry.h
+//  Free Food CMU
+//
+//  Created by Prashanth Balasubramaniam on 10/28/13.
+//  Copyright (c) 2013 Prashanth Balasubramaniam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FoodEntry : NSObject
+
+@property (nonatomic, strong) NSString *food;
+@property (nonatomic, strong) NSString *location;
+@property (nonatomic, strong) NSString *description;
+
+- (instancetype) initWithFood:(NSString *) food location:(NSString *) description:(NSString *) description;
+
+@end
---
+++ Free Food CMU/FoodEntry.m
@@ -0,0 +1,13 @@
+//
+//  FoodEntry.m
+//  Free Food CMU
+//
+//  Created by Prashanth Balasubramaniam on 10/28/13.
+//  Copyright (c) 2013 Prashanth Balasubramaniam. All rights reserved.
+//
+
+#import "FoodEntry.h"
+
+@implementation FoodEntry
+
+@end
