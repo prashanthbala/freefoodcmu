7b356174140ae4e2b1e5aa7637efd6947d73046a
Wale <adewale.desalu@gmail.com>
2013-10-28 19:38:28 -0400

    Added Classes and Variables

---
+++ Free Food CMU/FoodTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FoodTableViewController.h
+//  Free Food CMU
+//
+//  Created by Wale Desalu on 10/28/13.
+//  Copyright (c) 2013 Prashanth Balasubramaniam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodTableViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UILabel *FoodCellName;
+@property (strong, nonatomic) IBOutlet UILabel *FoodCellLocation;
+
+@end
