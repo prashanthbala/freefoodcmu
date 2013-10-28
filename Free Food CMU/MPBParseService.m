8e6b321cb78d531a58a9bc1a4a7f1c619a25f2d2
rasingh <rishabh@cmu.edu>
2013-10-28 02:56:26 -0400

    Added ParseService Object

---
+++ Free Food CMU/MPBParseService.m
@@ -0,0 +1,13 @@
+//
+//  MPBParseService.m
+//  Free Food CMU
+//
+//  Created by Rishabh Alaap Singh on 10/28/13.
+//  Copyright (c) 2013 Prashanth Balasubramaniam. All rights reserved.
+//
+
+#import "MPBParseService.h"
+
+@implementation MPBParseService
+
+@end
